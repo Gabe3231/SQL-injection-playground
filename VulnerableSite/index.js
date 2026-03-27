89697da5b35a7221cc602d1f406304281b187e83
Gabriel <dearaujo.c.gabriel@gmail.com>
2026-03-27 10:19:41 -0400

    database functional

--- VulnerableSite/index.js
+++ VulnerableSite/index.js
@@ -1,23 +1,17 @@
-// better detailed SQLite messages using verbose
-const sqlite3 = require("sqlite3").verbose();
-
-// path to db file
-const db = new sqlite3.Database('./users.db');
-
-// creating a very basic users table
-db.run(`
-  CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    Name TEXT,
-    Password TEXT
-  )
-`);
+// path to HTML and CSS files
+const path = require("path");
 
 // loading express to handle HTTP / requests
 const express = require('express');
 
 // handles middleware to read user input
 const bodyParser = require('body-parser');
+
+// better detailed SQLite messages using verbose
+const sqlite3 = require("sqlite3").verbose();
+
+// path to db file
+const db = new sqlite3.Database(path.join(__dirname, 'users.db'));
 
 // creating my server
 const weakSiteApp = express();
@@ -28,10 +22,17 @@ weakSiteApp.use(bodyParser.urlencoded({ extended: true }));
 // usable javascript object
 weakSiteApp.use(express.json());
 
-// path to HTML and CSS files
-const path = require("path");
 // to service all files in directory
 weakSiteApp.use(express.static(__dirname));
+
+// creating a very basic users table
+db.run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT,
+    Password TEXT
+  )
+`);
 
 // where user input is beinf take from
 weakSiteApp.post('/formSubmission', (req, res) => {
@@ -41,10 +42,13 @@ weakSiteApp.post('/formSubmission', (req, res) => {
     // made vulnerable query a constant
     const VulnerableSQL = `INSERT INTO users (Name, Password) VALUES ('${Name}', '${Password}')`;
     // db runs the vulnerable query
-    db.run(VulnerableSQL)
-    res.send("User added");
+    db.run(VulnerableSQL, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    })
+    res.send("User Added");
 });
 
 // listening on port 3000 which the server will run on
-weakSiteApp.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");});
+weakSiteApp.listen(3000, () => {console.log("Server running on http://localhost:3000");});
