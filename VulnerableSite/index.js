2f4b4140633664827a95f224592493d4ac8cd37f
Gabriel <dearaujo.c.gabriel@gmail.com>
2026-03-27 07:43:20 -0400

    DB table creation and express utilization

--- VulnerableSite/index.js
+++ VulnerableSite/index.js
@@ -1,7 +1,23 @@
 // better detailed SQLite messages using verbose
 const sqlite3 = require("sqlite3").verbose();
-// express was used for simpler coding
-const express = require("express");
 
-// used to define routes
-const app = express();
+// path to db file
+const db = new sqlite3.Database('./users.db');
+
+// creating a very basic users table
+db.run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT,
+    password TEXT
+  )
+`);
+
+// loading express to handle HTTP / requests
+const express = require('express');
+
+// handles middleware to read user input
+const bodyParser = require('body-parser');
+
+// creating my server
+const weakSiteApp = express();
